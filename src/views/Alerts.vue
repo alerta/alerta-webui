6259b96272dc30af43b042a02a724eefa7e958e0
Nick Satterly <nfsatterly@gmail.com>
2019-01-18 22:03:38 +0100

    Add environment tabs to alert list

--- src/views/Alerts.vue
+++ src/views/Alerts.vue
@@ -1,17 +1,71 @@
 <template>
   <div class="alerts">
-    <alert-list/>
+
+    <v-tabs
+      align-with-title
+      grow
+    >
+      <v-tabs-slider></v-tabs-slider>
+
+      <v-tab
+        v-for="env in environments"
+        :key="env"
+        :href="'#tab-' + env"
+        @click="setTab(env)"
+      >
+        {{ env }}
+      </v-tab>
+
+      <v-tabs-items>
+        <v-tab-item
+          v-for="env in environments"
+          :key="env"
+          :value="'tab-' + env"
+          :transition="false" :reverse-transition="false"
+        >
+          <alert-list :filter="filter"/>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-tabs>
+    
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+<script>
 import AlertList from '@/components/AlertList.vue'
 
-@Component({
+export default {
   components: {
     AlertList
+  },
+  data: () => ({
+    currentTab: 'ALL',
+    filter: null
+  }),
+  computed: {
+    environments() {
+      return ['ALL'].concat(this.$store.getters['alerts/environments'])
+    }
+  },
+  created() {
+    this.getEnvironments()
+    // this.getServices()
+    // this.getTags()
+  },
+  methods: {
+    getEnvironments() {
+      this.$store.dispatch('alerts/getEnvironments')
+    },
+    setTab(env) {
+      if (env === 'ALL') {
+        this.filter = {}
+      } else {
+        this.filter = { environment: env }
+      }
+    }
   }
-})
-export default class Alerts extends Vue {}
+}
 </script>
+
+<style>
+</style>
