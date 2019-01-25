aef4a72a5d9485e06253b7681b1092980047cb99
Nick Satterly <nfsatterly@gmail.com>
2019-01-25 15:37:49 +0100

    wip

--- src/views/Alerts.vue
+++ src/views/Alerts.vue
@@ -24,6 +24,13 @@
         </v-badge>
       </v-tab>
       <v-spacer></v-spacer>
+  <v-btn
+        color="purple"
+        dark
+        @click="sheet = true"
+      >
+        Click me
+      </v-btn>
       <v-btn
         flat
         icon
@@ -47,6 +54,27 @@
       </v-tabs-items>
     </v-tabs>
 
+    <v-bottom-sheet v-model="sheet">
+      <v-list>
+        <v-subheader>Open in</v-subheader>
+        <v-list-tile
+          v-for="tile in tiles"
+          :key="tile.title"
+          @click="sheet = false"
+        >
+          <v-list-tile-avatar>
+            <v-avatar size="32px" tile>
+              <img
+                :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`"
+                :alt="tile.title"
+              >
+            </v-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-title>{{ tile.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-bottom-sheet>
+
   </div>
 </template>
 
@@ -67,7 +95,15 @@ export default {
       service: null,
       status: ['open', 'ack']
     },
-    playSound: false
+    playSound: false,
+         sheet: false,
+      tiles: [
+        { img: 'keep.png', title: 'Keep' },
+        { img: 'inbox.png', title: 'Inbox' },
+        { img: 'hangouts.png', title: 'Hangouts' },
+        { img: 'messenger.png', title: 'Messenger' },
+        { img: 'google.png', title: 'Google+' }
+      ]
   }),
   computed: {
     environments() {
