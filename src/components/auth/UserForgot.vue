a9956671eb5c2ecbcb5387554d9d5c8d3f617483
Nick Satterly <nfsatterly@gmail.com>
2019-02-07 10:41:59 +0100

    Prompt user to return to sign in after password reset

--- src/components/auth/UserForgot.vue
+++ src/components/auth/UserForgot.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container grid-list-sm fill-height>
-    <v-layout align-center row wrap>
+    <v-layout v-show="!sent" align-center row wrap>
       <v-flex xs12 sm8 offset-xs0 offset-sm2>
         <p class="text-xs-center headline font-weight-medium">
           <span>Enter your email and we'll send you a reset link</span>
@@ -31,6 +31,32 @@
       </v-flex>
       <v-flex xs12 sm8 offset-xs0 offset-sm2></v-flex>
     </v-layout>
+
+    <v-layout v-show="sent" align-center row wrap>
+      <v-flex xs12 sm8 offset-xs0 offset-sm2>
+        <p class="text-xs-center headline font-weight-medium">
+          <span>Check your email for a reset link...</span>
+        </p>
+        <v-form @submit.prevent="forgot()">
+          <v-text-field
+            v-model="email"
+            name="login"
+            type="text"
+            label="Username"
+            prepend-inner-icon="alternate_email"
+            outline
+            readonly
+          >
+          </v-text-field>
+          <v-btn block color="primary" to="/login">Return to Sign In</v-btn>
+        </v-form>
+        <div class="text-xs-center">
+          <span class="body-2">Already have an account?</span>
+          <v-btn flat color="primary" to="/login">Sign In</v-btn>
+        </div>
+      </v-flex>
+      <v-flex xs12 sm8 offset-xs0 offset-sm2></v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -41,7 +67,8 @@ export default {
   props: [],
   data: () => ({
     email: null,
-    message: null
+    message: null,
+    sent: false
   }),
   computed: {
     isSending() {
@@ -56,6 +83,7 @@ export default {
           'Reset email sent successfully!',
           { root: true }
         )
+        this.sent = true
       })
     }
   }
