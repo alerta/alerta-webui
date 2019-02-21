0dd7cc09b306e76466a0376ebcdcd31a486fb36f
Nick Satterly <nfsatterly@gmail.com>
2019-02-21 19:19:59 +0100

    Add custom alert actions to "more vert" drop down menu

---
+++ src/filters/splitCaps.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+export default Vue.filter('splitCaps', function(value) {
+  if (!value) return ''
+  return value.toString().replace(/([A-Z])/g, ' $1').split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ')
+})
