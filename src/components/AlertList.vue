97af3d99e5391ce38ce8b7698329ab4b8925a323
Nick Satterly <nfsatterly@gmail.com>
2019-05-13 14:38:15 +0200

    Use v-html for alert text so html links work

--- src/components/AlertList.vue
+++ src/components/AlertList.vue
@@ -122,7 +122,7 @@
               v-if="col == 'text'"
               class="text-no-wrap"
             >
-              {{ props.item.text }}
+              <span v-html="props.item.text"></span>
             </span>
             <span
               v-if="col == 'tags'"
@@ -222,7 +222,7 @@
           >
             <div class="fixed-table">
               <div class="text-truncate">
-                {{ props.item.text }}
+                <span v-html="props.item.text"></span>
               </div>
             </div>
           </td>
