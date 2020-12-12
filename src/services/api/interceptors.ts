272db9ed3714928ab7475e2fe212adb81efa6aed
Nick Satterly <nfsatterly@gmail.com>
2020-12-13 00:27:04 +0100

    fix: add helpful error message if susupected CORS issue (#420)

--- src/services/api/interceptors.ts
+++ src/services/api/interceptors.ts
@@ -1,5 +1,6 @@
 import { store } from '@/main'
 import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
 
 export function makeInterceptors(router) {
   return {
@@ -11,6 +12,18 @@ export function makeInterceptors(router) {
 
     // response handlers
     interceptErrors(error) {
+      if (!error.response && !axios.isCancel(error)) {
+        alert(
+          'ERROR: A network error occurred. This could be a CORS issue or a ' +
+          'dropped internet connection.\n\n' +
+          'Check the browser javascript console and if the HTTP request has ' +
+          'been blocked by CORS then ensure that the "X-Request-ID" ' +
+          'header is in the "CORS_ALLOW_HEADERS" list in the Alerta API ' +
+          'configuration, or upgrade the Alerta API server to version 8.3.0 or ' +
+          'later.'
+        )
+      }
+
       if (error.response) {
         store.dispatch('notifications/error', error.response.data)
       }
