4cc59d96b3b56569e351d0f06c7a28a08d87c076
zer0def <zer0def@zer0def.0>
2020-12-31 14:58:46 +0100

    Allow usage of relative paths for base URLs to aid subpathed reverse proxies
    Related: alerta/docker-alerta#177

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-  publicPath: process.env.BASE_URL,
+  publicPath: '',
   chainWebpack: config => {
     config.module
       .rule('fonts')
