f245c31884ce12b9ae4e5445ab77a08b6b0f541f
Nick Satterly <nfsatterly@gmail.com>
2021-01-07 13:11:26 +0100

    fix: support display density setting for all users (#442)

--- src/components/auth/ProfileMe.vue
+++ src/components/auth/ProfileMe.vue
@@ -223,6 +223,7 @@ export default {
   },
   methods: {
     logout() {
+      this.$store.dispatch('resetUserPrefs')
       this.$store
         .dispatch('auth/logout')
         .then(response => {
