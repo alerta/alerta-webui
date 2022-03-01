2f5c279dca9d254b6645dba781a73a7a6ed7f577
Victor Gorchilov <victor@gorchilov.com>
2022-03-01 11:31:49 +0200

    [up] unit test config, github action for release

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
       'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
