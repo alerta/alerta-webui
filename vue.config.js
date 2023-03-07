6770fe6c0ce96bcffab7cdf3a0b9079dc8fdf964
headphonejames <generalfuzz@gmail.com>
2023-03-07 13:50:37 -0800

    Upgraded @vue/cli* packages
    -The "vue upgrade" command should do this but it doesn't seem to be working

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,6 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 module.exports = {
   publicPath: process.env.BASE_URL,
   chainWebpack: config => {
-    //Remove this when compatibility mode is no longer needed
-    config.resolve.alias.set('vue', '@vue/compat')
     config.module
       .rule('fonts')
       .use('url-loader')
@@ -13,21 +11,8 @@ module.exports = {
         limit: 4096,
         name: 'fonts/[name].[ext]'
       })
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
   },
   devServer: {
-    disableHostCheck: true
+    allowedHosts: 'all'
   }
 }
