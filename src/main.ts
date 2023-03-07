9084fa39ff0d44bc7892bab0c5511c15d032b665
headphonejames <generalfuzz@gmail.com>
2023-03-07 13:46:02 -0800

    Exported app instance from main.ts. Readded missing npm packages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import bootstrap from './services/config'
 
-import Vue from 'vue'
+import {createApp} from 'vue'
 
 import {createStore} from './store'
 import {createRouter} from './router'
@@ -29,10 +29,10 @@ import '@/filters/until'
 
 export const store = createStore()
 
+let app;
 bootstrap.getConfig().then(config => {
   const router = createRouter(config.base_path)
 
-  Vue.prototype.$config = config
   store.dispatch('updateConfig', config)
   store.dispatch('alerts/setFilter', config.filter)
   store.registerModule('auth', makeStore(vueAuth(config)))
@@ -43,16 +43,21 @@ bootstrap.getConfig().then(config => {
   axios.interceptors.response.use(undefined, interceptors.interceptErrors)
   axios.interceptors.response.use(undefined, interceptors.redirectToLogin)
 
-  Vue.use(GoogleAnalytics, {
+  app = createApp({
+    router,
+    store,
+    i18n,
+    render: (h:any) => h(App)
+  })
+  app.config.globalProperties.$config = config
+
+  app.use(GoogleAnalytics, {
     trackingId: config.tracking_id,
     router
   })
   sync(store, router)
 
-  new Vue({
-    router,
-    store,
-    i18n,
-    render: (h: any) => h(App)
-  }).$mount('#app')
+  app.mount('#app')
 })
+
+export default app;
