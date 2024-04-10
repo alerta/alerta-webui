6c190b65ed50c5ab0829bd1f78e2fc8357bb935d
Nick Satterly <nfsatterly@gmail.com>
2024-04-10 13:41:55 +0200

    test user list

--- tests/unit/components/UserList.spec.ts
+++ tests/unit/components/UserList.spec.ts
@@ -1,9 +1,17 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import UserList from '@/components/UserList.vue'
 
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
 describe('UserList', () => {
+
+  let store = new Vuex.Store({})
+
   it('renders a div', () => {
-    const wrapper = shallowMount(UserList)
+    const wrapper = shallowMount(UserList, { store, localVue })
     expect(wrapper.contains('div')).toBe(true)
   })
 })
